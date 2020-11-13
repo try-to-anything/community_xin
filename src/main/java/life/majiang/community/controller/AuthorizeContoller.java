72b5315dcccad5a8c2e868605f21d25be202b44b
try-to-anything <47705165+try-to-anything@users.noreply.github.com>
2020-11-13 23:02:47 +0800

    sesstion

--- src/main/java/life/majiang/community/controller/AuthorizeContoller.java
+++ src/main/java/life/majiang/community/controller/AuthorizeContoller.java
@@ -5,8 +5,12 @@ import life.majiang.community.dto.GithubUser;
 import life.majiang.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author songjian
@@ -15,12 +19,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class AuthorizeContoller {
 
-//    @Autowired
+    @Autowired
     private GithubProvider githubProvider;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
@@ -28,7 +33,12 @@ public class AuthorizeContoller {
         accessTokenDTO.setClient_secret("fb0632b756ef53bdc95d04cb17be50626dca0f62");
         String token = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(token);
-        System.out.println(user.getName());
-        return "index";
+        if(user != null){
+           //登录成功，写cookie和seesion
+            request.getSession().setAttribute("user",user);//这里将user设置了一个session，统计在线人数？
+            return "redirect:/";
+        }else{
+            return "redirect:index";
+        }
     }
 }
